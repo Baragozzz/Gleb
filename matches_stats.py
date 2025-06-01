9f166cea029ef619aea97a54f5389bce7363a08e
Baragozzz <69815126+Baragozzz@users.noreply.github.com>
2025-06-01 22:57:53 +0400

    Update matches_stats.py

--- matches_stats.py
+++ matches_stats.py
@@ -1,18 +1,18 @@
 import streamlit as st
+import asyncio
 from pages.statistics import statistics_page
 from pages.roster import roster_page
 
 def main():
     st.title("11x11 Статистика")
 
-    # Создание вкладок
     tab1, tab2 = st.tabs(["Статистика матчей", "Ростер игроков"])
 
     with tab1:
-        statistics_page()  # Вызов страницы статистики матчей
+        asyncio.run(statistics_page())  # Теперь вызываем асинхронно!
 
     with tab2:
-        roster_page()  # Вызов страницы роста игроков
+        roster_page()
 
 if __name__ == "__main__":
     main()
