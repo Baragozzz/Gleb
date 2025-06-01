f0f7fa704c82929e2fe1db5459e3c92859afeada
Baragozzz <69815126+Baragozzz@users.noreply.github.com>
2025-06-01 22:56:37 +0400

    Update data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,3 +1,3 @@
-def async_main(mode_choice, target_url, filter_from, filter_to, login, password):
-    # Вставьте сюда вашу существующую логику async_main из matches_stats.py
+async def async_main(mode_choice, target_url, filter_from, filter_to, login, password):
+    # Ваш код асинхронного анализа матчей
     pass
