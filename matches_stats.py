6b69b6bd898daebf1d74a1f930e4e4cee4ac723d
Baragozzz <69815126+Baragozzz@users.noreply.github.com>
2025-06-02 17:35:57 +0400

    Update matches_stats.py

--- matches_stats.py
+++ matches_stats.py
@@ -2,19 +2,34 @@ import streamlit as st
 from pages.statistics import statistics_page
 from pages.roster import roster_page
 import os
+
+# Если требуется установить playwright (однократно), можно оставить эту строку:
 os.system("playwright install")
+
+def home_page():
+    st.title("Добро пожаловать в 11x11 Статистика!")
+    st.markdown("""
+    Это приложение предназначено для анализа и мониторинга статистики 11x11.
+
+    **Разделы приложения:**
+    - **Статистика матчей:** Анализируйте результаты матчей для отдельных профилей или союзов.
+    - **Ростер игроков:** Просматривайте состав союза, а также метрики вроде *Сила 11 лучших* и *Ср. сила 11 лучших*.
+
+    Используйте вкладки ниже для навигации по разделам.
+    """)
 
 def main():
     st.title("11x11 Статистика")
-
-    # Создание вкладок
-    tab1, tab2 = st.tabs(["Статистика матчей", "Ростер игроков"])
-
-    with tab1:
-        statistics_page()  # Вызов страницы статистики матчей
-
-    with tab2:
-        roster_page()  # Вызов страницы роста игроков
+    
+    # Создаем три вкладки: домашняя, статистика матчей и ростер игроков.
+    tab_home, tab_stats, tab_roster = st.tabs(["Домашняя", "Статистика матчей", "Ростер игроков"])
+    
+    with tab_home:
+        home_page()
+    with tab_stats:
+        statistics_page()
+    with tab_roster:
+        roster_page()
 
 if __name__ == "__main__":
     main()
